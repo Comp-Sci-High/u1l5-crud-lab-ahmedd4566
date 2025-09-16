cc6f3eb0f52dc76898f31ce93129ee26b76e92ae
67 ahmed <ahmed.daiyan26@compscihigh.org>
2025-09-16 18:45:08 +0000

    task 1-4 done

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // ARRAY SECTION
 // Task 1: Create an empty array called playlist
-
+let playlist = []
 
 // Task 2: Push 3 songs to the playlist array
-
+playlist.push('March Madness', 'Labubu Phonk', 'Just Dance')
 
 // Task 3: Update the value of the last element to be "Test"
-
+playlist[2]='Test'
 
 // Task 4: Delete the "Test" element 
-
+playlist.pop
 // Console log the array
-
+console.log(playlist)
 
 // Commit your changes with an appropriate message.
 // ----------------------------------
